4fb3f6c8e1d03a66928824e3271efbeaea3701d4
Martin Rydén <pemryd@gmail.com>
2017-10-31 14:55:06 +0100

    Update random.walk.R

--- random.walk.R
+++ random.walk.R
@@ -5,11 +5,14 @@ wlen = as.integer(readline(prompt="How far should we walk? "))
 wstep = as.integer(readline(prompt="How big should are steps be? "))
 
 rwalk <- function(wlen) {
+  last = 0
   while (i <= wlen) {
-    w <- c(w, rnorm(wstep)) # Append random seed to vector
+    last <- last + rnorm(wstep)
+    w <- c(w, last) # Append random seed to vector
     i <- i + 1 # Increment counter
   }
   return(w)
 }
 
 p <- plot(rwalk(wlen), type='b')
+
